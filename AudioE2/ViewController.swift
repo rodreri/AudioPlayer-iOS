b9fdd0178b574afa8245a4bbbee1fa0d137ee5a7
erick <rodreri>
2022-11-05 10:39:57 -0600

    Initial Commit

---
+++ AudioE2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AudioE2
+//
+//  Created by Erick Rodrigo Minero Pineda on 05/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
